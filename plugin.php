c94f003256f7a6b8acfaa7d89fa053773e647e0d
Robert O'Rourke <robert@o-rourke.org>
2020-12-17 15:33:33 +0000

    Be more strict about location of rest prefix

--- plugin.php
+++ plugin.php
@@ -14,6 +14,6 @@ require_once __DIR__ . '/inc/namespace.php';
 add_action( 'init', __NAMESPACE__ . '\\redirect_user', 999 );
 
 // Ensure application passwords can be verified early for REST API requests.
-if ( strpos( $_SERVER['REQUEST_URI'], '/' . rest_get_url_prefix() ) !== false ) {
+if ( strpos( $_SERVER['REQUEST_URI'], '/' . rest_get_url_prefix() ) === 0 ) {
 	add_filter( 'application_password_is_api_request', '__return_true' );
 }
