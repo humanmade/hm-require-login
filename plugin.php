95434c23e7877ec7d215e36ee1c73f3f23198680
Robert O'Rourke <robert@o-rourke.org>
2020-12-17 14:22:54 +0000

    Change the hook to `template_redirect`
    The `template_redirect` action is a more appropriate hook for this functionality.
    
    Specifically however with the addition of application passwords in WordPress 5.6 the `init` is too early for the REST API server to have been instantiated or for the application password to have been processed in a REST API request context.

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
 Plugin Name: HM Require Login
 Description: Only allow the site to be accessed by logged in users.
 Author: Human Made Limited
-Version: 1.0.3
+Version: 1.0.4
 Author URI: http://hmn.md
 */
 
@@ -12,4 +12,4 @@ namespace HM\Require_Login;
 
 require_once __DIR__ . '/inc/namespace.php';
 
-add_action( 'init', __NAMESPACE__ . '\\redirect_user', 999 );
+add_action( 'template_redirect', __NAMESPACE__ . '\\redirect_user', 1 );
